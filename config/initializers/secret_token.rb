ae3891844e766dd9fa69945e9713c92cc5e2f561
Nathaniel Barnes <kerianambrai@gmail.com>
2012-05-11 17:45:49 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lfgame::Application.config.secret_token = '6d93480fa12718b3aaee0042b0fdef92404113c466e2d6584cfaf9084ee19718cc3e10745930d3246faab6749aa3796b06599833471e30d657e52d74a171029a'
