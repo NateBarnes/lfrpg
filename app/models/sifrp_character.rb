c009cd737ef7b60e2c767f952dd895f9340b7e20
Nathaniel Barnes <kerianambrai@gmail.com>
2012-05-16 18:45:40 -0500

    Updated tests and added generation of qualities

--- app/models/sifrp_character.rb
+++ app/models/sifrp_character.rb
@@ -1,8 +1,18 @@
 class SIFRPCharacter < Character
   ABILITIES = [:agility, :animal_handling, :athletics, :awareness, :endurance, :fighting, :marksmanship, :status, :will, :all_others]
-  initialize_vars ABILITIES
+  QUALITIES = [:exp, :destiny]
+  initialize_vars ABILITIES+QUALITIES
+
+  def self.generate
+    char = create
+    char.generate
+  end
 
   def generate
     ABILITIES.each { |ability| vars[ability] = 2 }
+    vars[:exp] = 210
+    vars[:destiny] = 1
+
+    self
   end
 end
--- test/unit/sifrp_character_test.rb
+++ test/unit/sifrp_character_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class SIFRPCharacterTest < ActiveSupport::TestCase
-  test "true" do
-    assert true
+  ABILITIES = [:agility, :animal_handling, :athletics, :awareness, :endurance, :fighting, :marksmanship, :status, :will, :all_others]
+
+  def test_generate
+    char = SIFRPCharacter.generate
+    ABILITIES.each do |ability|
+      assert_equal char.send(ability), 2
+    end
+    assert_equal char.exp, 210
+    assert_equal char.destiny, 1
   end
 end
